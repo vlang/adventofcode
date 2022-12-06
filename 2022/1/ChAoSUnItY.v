a2a36107bf78fe4c3e2cab22a26ac22c889e0e62
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-12-06 19:04:03 +0800

    add solution for Day 01

---
+++ 2022/1/ChAoSUnItY.v
@@ -0,0 +1,26 @@
+import os { read_file }
+import arrays { max, sum }
+
+fn main() {
+	inputs := read_file('calories.input')!
+	data := process_data(inputs)!
+
+	println(part1(data)!)
+	println(part2(data)!)
+}
+
+fn part1() !int {
+	return max(data)!
+}
+
+fn part2() !int {
+	return sum(data[0..3])!
+}
+
+fn process_data(data string) ![]int {
+	mut result := data.replace('\r\n', '\n')
+		.split('\n\n')
+		.map(sum(it.split('\n').map(it.int()))!)
+	result.sort(b < a)
+	return result
+}
