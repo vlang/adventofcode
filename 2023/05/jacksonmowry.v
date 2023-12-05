3dc7dc7dcb6d70cd0e3e49e3c084af22435009c4
Jackson Mowry <jacksonm@duck.com>
2023-12-05 13:23:36 -0500

    Day 5, Part 1, Part 2

--- 2023/05/jacksonmowry.v
+++ 2023/05/jacksonmowry.v
@@ -2,11 +2,10 @@ module main
 
 import os
 import arrays
-// import math
+import math
 
 fn main() {
-	// input := os.read_file('seed_map.input')!
-	input := os.read_file('personal.input')!
+	input := os.read_file('seed_map.input')!
 		.split('\n\n')
 		.map(it.split_into_lines().map(it.trim_space()))
 	mut seeds := (input[0][0].split(': ')[1]).split(' ').map(it.i64())
@@ -24,18 +23,26 @@ fn main() {
 
 	println('Part 1: ${arrays.min(seeds)!}')
 
-	mut seeds_2 := (input[0][0].split(': ')[1]).split(' ').map(it.i64())
-	mut part_2_min := max_i64
-	mut maps := [][][]i64{}
+	mut seeds_2 := (input[0][0].split(': ')[1]).split(' ').map(it.u64())
+	mut part_2_min := max_u64
+	mut maps := [][][]u64{}
 	for i in 1 .. 8 {
-		maps << input[i][1..].map(it.split(' ').map(it.i64()))
+		maps << input[i][1..].map(it.split(' ').map(it.u64()))
 	}
-	for i := 0; i < seeds_2 - 2; i++ {
-		start := seeds_2[i]
-		end := start + seeds_2[i + 1] + 1
-		for ; start < end; i++ {
+	for i := 0; i < seeds_2.len - 1; i += 2 {
+		end := seeds_2[i] + seeds_2[i + 1] - 1
+		for start := seeds_2[i]; start <= end; start++ {
+			mut copy := start
+			for mapx in maps {
+				for line in mapx {
+					if copy >= line[1] && copy < line[1] + line[2] {
+						copy = line[0] + copy - line[1]
+						break
+					}
+				}
+			}
+			part_2_min = math.min(part_2_min, copy)
 		}
 	}
 	println('Part 2: ${part_2_min}')
-	println('done')
 }
