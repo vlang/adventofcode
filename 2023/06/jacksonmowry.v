abdddb314c11c30ef569d5591ac09a789ef3b0a4
JingerJack <tryhardsax@gmail.com>
2023-12-06 09:39:33 -0500

    Combine pt1 & pt2

--- 2023/06/jacksonmowry.v
+++ 2023/06/jacksonmowry.v
@@ -5,14 +5,17 @@ import math
 
 fn main() {
 	lines := os.read_lines('race_times.input')!
-	times := lines[0].split(':')[1].split(' ').filter(it != '').map(it.int())
-	distances := lines[1].split(':')[1].split(' ').filter(it != '').map(it.int())
+	mut times := lines[0].split(':')[1].split(' ').filter(it != '').map(it.u64())
+	times << lines[0].split(':')[1].split('').filter(it != ' ').join('').u64()
+	mut distances := lines[1].split(':')[1].split(' ').filter(it != '').map(it.u64())
+	distances << lines[1].split(':')[1].split('').filter(it != ' ').join('').u64()
 	mut product := 1
+	mut part_2 := u64(0)
 
 	for i := 0; i < times.len; i++ {
 		a := f64(-1)
 		b := f64(times[i])
-		c := f64(-distances[i])
+		c := f64(-1) * f64(distances[i])
 		discriminant := (b * b) - (4 * a * c)
 		if discriminant == 0 {
 			continue
@@ -27,29 +30,13 @@ fn main() {
 				upper -= 1
 			}
 			upper = math.floor(upper)
-			product *= int(math.abs(upper - lower)) + 1
+			if i == times.len - 1 {
+				part_2 = u64(math.abs(upper - lower)) + 1
+			} else {
+				product *= int(math.abs(upper - lower)) + 1
+			}
 		}
 	}
 	println('Part 1: ${product}')
-
-	time := lines[0].split(':')[1].split('').filter(it != ' ').join('').u64()
-	distance := lines[1].split(':')[1].split('').filter(it != ' ').join('').u64()
-	a := f64(-1)
-	b := f64(time)
-	c := f64(-1) * f64(distance)
-	discriminant := (b * b) - (4 * a * c)
-	if discriminant == 0 {
-		panic('Sqrt of 0')
-	}
-	mut lower := (-b + math.sqrt(discriminant)) / (2 * a)
-	if math.ceil(lower) == lower {
-		lower += 1
-	}
-	lower = math.ceil(lower)
-	mut upper := (-b - math.sqrt(discriminant)) / (2 * a)
-	if math.ceil(upper) == upper {
-		upper -= 1
-	}
-	upper = math.floor(upper)
-	println('Part 2: ${u64(math.abs(upper - lower) + 1)}')
+	println('Part 2: ${part_2}')
 }
