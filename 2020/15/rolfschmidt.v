369e5f556644c810fd5d952f760ef51dd8c73121
Rolf Schmidt <rolf@schmidt.ink>
2023-12-14 18:06:10 +0100

    fixes 2020/15/rolfschmidt - GC Warning: Repeated allocation of very large block (#45)
    Hi,
    i added an optimized version of that day to fix the error ;)

--- 2020/15/rolfschmidt.v
+++ 2020/15/rolfschmidt.v
@@ -6,33 +6,28 @@ import os
 
 @[inline]
 fn d15_run(lines string, find int) int {
-	mut spoken_before := map[int]int{}
-	mut spoken := map[int]int{}
-	mut numbers := lines.split(',')
-	for i, number in numbers {
-		spoken[number.int()] = i
+	mut numbers := lines.split(',').map(it.int())
+	mut last_number := numbers.last()
+	mut position := 0
+	mut positions := []int{len: find}
+	for number in numbers {
+		position += 1
+		positions[number] = position
 	}
-	mut current_number := 0
-	mut last_number := numbers.last().int()
-	mut last_index := 0
-	mut last_index2 := 0
-	for i := numbers.len - 1; i < find - 1; i++ {
-		current_number = last_number
-		if current_number !in spoken {
-			last_number = 0
-			spoken[current_number] = i
-		} else {
-			spoken_before[current_number] = spoken[current_number]
-			spoken[current_number] = i
-			last_index = spoken[current_number]
-			last_index2 = spoken_before[current_number]
-			if last_index == i && last_index2 == i - 1 {
-				last_number = 1
-			} else {
-				last_number = ((last_index + 1) - (last_index2 + 1))
-			}
+
+	for position < find {
+		last_position := positions[last_number]
+
+		mut next_number := 0
+		if last_position != 0 {
+			next_number = position - last_position
 		}
+
+		positions[last_number] = position
+		position += 1
+		last_number = next_number
 	}
+
 	return last_number
 }
 
