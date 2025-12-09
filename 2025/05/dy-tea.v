132b80c15d77abe41049c4a3faefb08098086ba0
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-12-09 00:15:52 +0000

    run `v fmt -w .`

--- 2025/05/dy-tea.v
+++ 2025/05/dy-tea.v
@@ -4,34 +4,34 @@ import arrays
 lines := os.read_lines('ingredients.input')!
 mut sep_idx := 0
 for i, line in lines {
-  if line == '' {
-    sep_idx = i
-    break
-  }
+	if line == '' {
+		sep_idx = i
+		break
+	}
 }
 ranges := lines[..sep_idx].map(fn (x string) []u64 {
-  left, right := x.split_once('-') or { panic('invalid input') }
-  return [left.u64(), right.u64()]
+	left, right := x.split_once('-') or { panic('invalid input') }
+	return [left.u64(), right.u64()]
 })
-available := lines[sep_idx+1..].map(|x| x.u64())
+available := lines[sep_idx + 1..].map(|x| x.u64())
 
 // part 1
 mut sum := u64(0)
 for id in available {
-  for rn in ranges {
-    left, right := rn[0], rn[1]
-    if id >= left && id <= right {
-      sum++
-      break
-    }
-  }
+	for rn in ranges {
+		left, right := rn[0], rn[1]
+		if id >= left && id <= right {
+			sum++
+			break
+		}
+	}
 }
 println(sum)
 
 // part 2
 mut merged := [][]u64{}
 for rn in ranges {
-  mut nl := rn[0]
+	mut nl := rn[0]
 	mut nr := rn[1]
 	mut removed := []int{}
 	for i, ex in merged {
--- 2025/06/dy-tea.v
+++ 2025/06/dy-tea.v
@@ -6,24 +6,24 @@ mut multiple_spaces := re.regex_opt(' +')!
 file := os.read_file('worksheet.input')!.split_any('\n\r')
 mut lines := [][]string{}
 for line in file {
-  lines << multiple_spaces.split(line).filter(|s| s != '')
+	lines << multiple_spaces.split(line).filter(|s| s != '')
 }
 ops := lines.last()
 
 // part 1
 mut sum := u64(0)
-for i in 0..ops.len {
-  mut tmp := lines[0][i].u64()
-  if ops[i] == '+' {
-    for j in 1..lines.len - 1 {
-      tmp += lines[j][i].u64()
-    }
-  } else {
-    for j in 1..lines.len - 1 {
-      tmp *= lines[j][i].u64()
-    }
-  }
-  sum += tmp
+for i in 0 .. ops.len {
+	mut tmp := lines[0][i].u64()
+	if ops[i] == '+' {
+		for j in 1 .. lines.len - 1 {
+			tmp += lines[j][i].u64()
+		}
+	} else {
+		for j in 1 .. lines.len - 1 {
+			tmp *= lines[j][i].u64()
+		}
+	}
+	sum += tmp
 }
 println(sum)
 
@@ -32,22 +32,22 @@ sum = 0
 file_last := file.last()
 mut digits := []u64{}
 for j := file_last.len - 1; j >= 0; j-- {
-  curr := file_last[j]
-  mut st := ''
-  for i in 0..file.len - 1 {
-    st += file[i][j].ascii_str()
-  }
-  dig := st.replace(' ', '').u64()
-  if dig != 0 {
-    digits << dig
-  }
-  if curr != ` ` {
-    if curr == `+` {
-      sum += arrays.sum[u64](digits)!
-    } else {
-      sum += arrays.fold[u64, u64](digits, 1, |acc, elem| acc * elem)
-    }
-    digits.clear()
-  }
+	curr := file_last[j]
+	mut st := ''
+	for i in 0 .. file.len - 1 {
+		st += file[i][j].ascii_str()
+	}
+	dig := st.replace(' ', '').u64()
+	if dig != 0 {
+		digits << dig
+	}
+	if curr != ` ` {
+		if curr == `+` {
+			sum += arrays.sum[u64](digits)!
+		} else {
+			sum += arrays.fold[u64, u64](digits, 1, |acc, elem| acc * elem)
+		}
+		digits.clear()
+	}
 }
 println(sum)
